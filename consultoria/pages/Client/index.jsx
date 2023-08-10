21b332cc3374291c3e709ce9c9ed9d5b4b750111
Ana Santos <ana.br@hotmail.com>
2023-08-10 10:15:11 -0300

    correção da busca por razão social pag de clientes

--- consultoria/pages/Client/index.jsx
+++ consultoria/pages/Client/index.jsx
@@ -1,9 +1,10 @@
+
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Profile from '../Profile';
 import EntrepriseProfile from '../EnterpriseProfile';
 
-const Client = () => {
+const Client = ({clientsProp  }) => {
   const clients = [
     {
       id: 1,
@@ -12,7 +13,7 @@ const Client = () => {
       mobile: 81988693599,
       entreprise: [
         {
-          cpfCnpj: 12345678910,
+          cpfCnpj: 12345678910000,
           razaoSocial: "Edficações LTDA",
           endereco: "Rua do jasmin",
           telefone: "81-9887692454",
@@ -29,7 +30,7 @@ const Client = () => {
           ],
         },
         {
-          cpfCnpj: 222222222,
+          cpfCnpj: 22222222222222,
           razaoSocial: "Tech Team LTDA",
           endereco: "Rua do Leme",
           telefone: "81-9887692454",
@@ -54,7 +55,7 @@ const Client = () => {
       mobile: 81988693599,
       entreprise: [
         {
-          cpfCnpj: 4444444,
+          cpfCnpj: 44444444444444,
           razaoSocial: "Stefanini",
           endereco: "Rua do Cravo",
           telefone: "81-9887692454",
@@ -71,7 +72,7 @@ const Client = () => {
           ],
         },
         {
-          cpfCnpj: 3333333,
+          cpfCnpj: 33333333333333,
           razaoSocial: "Globo",
           endereco: "Rua do Amarelo",
           telefone: "81-9887692454",
@@ -96,7 +97,7 @@ const Client = () => {
       mobile: 81988693599,
       entreprise: [
         {
-          cpfCnpj: 888888888,
+          cpfCnpj: 88888888888888,
           razaoSocial: "Oi Telecom",
           endereco: "Rua da Alegria",
           telefone: "81-9887692454",
@@ -113,7 +114,7 @@ const Client = () => {
           ],
         },
         {
-          cpfCnpj: 777777777,
+          cpfCnpj: 77777777777777,
           razaoSocial: "Vivo S/A",
           endereco: "Rua Paralela",
           telefone: "81-9887692454",
@@ -138,7 +139,7 @@ const Client = () => {
       mobile: 81988693599,
       entreprise: [
         {
-          cpfCnpj: 4444444,
+          cpfCnpj: 99999999999999,
           razaoSocial: "Fync",
           endereco: "Rua do Cravo",
           telefone: "81-9887692454",
@@ -155,7 +156,7 @@ const Client = () => {
           ],
         },
         {
-          cpfCnpj: 3333333,
+          cpfCnpj: 12593570298715,
           razaoSocial: "Globo",
           endereco: "Rua do Amarelo",
           telefone: "81-9887692454",
@@ -176,89 +177,92 @@ const Client = () => {
     // Adicionar outros clientes aqui...
   ];
   
-
-  const [searchTerm, setSearchTerm] = useState('');
-  const [expandedClients, setExpandedClients] = useState(
-    clients.reduce((acc, client) => {
-      acc[client.id] = true;
-      return acc;
-    }, {})
-  );
-  const [expandedEnterpriseIndex, setExpandedEnterpriseIndex] = useState(-1);
-
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
-    setExpandedClients((prevExpandedClients) => ({})); // Reset all client accordions on search
-    setExpandedEnterpriseIndex(-1); // Reset expanded enterprise accordion on search
-  };
-
-  const toggleClientAccordion = (clientId) => {
-    setExpandedClients((prevExpandedClients) => ({
-      ...prevExpandedClients,
-      [clientId]: !prevExpandedClients[clientId],
-    }));
-    setExpandedEnterpriseIndex(-1); // Reset expanded enterprise accordion on client accordion click
-  };
-
-  const handleEnterpriseAccordionClick = (index) => {
-    setExpandedEnterpriseIndex(index === expandedEnterpriseIndex ? -1 : index);
-  };
-
-  return (
-    <div>
-      <Link to="/home">
-        <button>Home</button>
-      </Link>
-      <h1>Página do Cliente</h1>
+  
+    const [searchTerm, setSearchTerm] = useState('');
+    const [expandedClients, setExpandedClients] = useState(
+      clients.reduce((acc, client) => {
+        acc[client.id] = true;
+        return acc;
+      }, {})
+    );
+    const [expandedEnterpriseIndex, setExpandedEnterpriseIndex] = useState(-1);
+  
+    const handleSearch = (event) => {
+      setSearchTerm(event.target.value);
+      setExpandedClients({}); // Reset all client accordions on search
+      setExpandedEnterpriseIndex(-1); // Reset expanded enterprise accordion on search
+    };
+  
+    const toggleClientAccordion = (clientId) => {
+      setExpandedClients((prevExpandedClients) => ({
+        ...prevExpandedClients,
+        [clientId]: !prevExpandedClients[clientId],
+      }));
+      setExpandedEnterpriseIndex(-1); // Reset expanded enterprise accordion on client accordion click
+    };
+  
+    const handleEnterpriseAccordionClick = (index) => {
+      setExpandedEnterpriseIndex(index === expandedEnterpriseIndex ? -1 : index);
+    };
+  
+    const filterClients = clients.filter((client) =>
+      client.entreprise.some((enterprise) =>
+        enterprise.razaoSocial.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+  
+    return (
       <div>
-        <input
-          type="text"
-          placeholder="Buscar por nome..."
-          value={searchTerm}
-          onChange={handleSearch}
-        />
-      </div>
-      {clients.map((client) => (
-        <div key={client.id} style={{ marginBottom: '10px', border: '1px solid #ccc', padding: '10px' }}>
-          <button onClick={() => toggleClientAccordion(client.id)}>
-            {client.name} <Link to={`/profile/${client.id}`}>
-                <button>Ver Perfil</button>
-              </Link> {expandedClients[client.id] ? '▲' : '▼'}
-          </button>
-          {expandedClients[client.id] && (
-            <div>
-              {/* <p>Email: {client.email}</p>
-              <p>Telefone: {client.mobile}</p> */}
-              
-              {/* <Link to={`/edit/${client.id}`}>
-                <button>Editar Dados</button>
-              </Link> */}
-              <h2>Empresas:</h2>
-              {client.entreprise.map((enterprise, index) => (
-                <div key={index}>
-                  <button onClick={() => handleEnterpriseAccordionClick(index)}>
-                    {enterprise.razaoSocial}{' '}
-                    {expandedEnterpriseIndex === index ? '▲' : '▼'}
-                  </button>
-                  {expandedEnterpriseIndex === index && (
-                    <div>
-                      <p>CNPJ: {enterprise.cpfCnpj}</p>
-                      {/* <p>Razão Social: {enterprise.razaoSocial}</p>
-                      <p>Endereço: {enterprise.endereco}</p>
-                      <p>Telefone: {enterprise.telefone}</p> */}
-                      <Link to={`/entrepriseProfile/${enterprise.cpfCnpj}`}>
-      <button>Ver Perfil da Empresa</button>
-    </Link>
-                    </div>
-                  )}
-                </div>
-              ))}
-            </div>
-          )}
+        <Link to="/home">
+          <button>Home</button>
+        </Link>
+        <h1>Página do Cliente</h1>
+        <div>
+        <label htmlFor="search">Buscar por Razão Social:</label>
+          <input
+            type="text"
+            placeholder="Buscar por razão social..."
+            value={searchTerm}
+            onChange={handleSearch}
+          />
         </div>
-      ))}
-    </div>
-  );
-};
-
-export default Client;
+        {filterClients.map((client) => (
+          <div
+            key={client.id}
+            style={{ marginBottom: '10px', border: '1px solid #ccc', padding: '10px' }}
+          >
+            <button onClick={() => toggleClientAccordion(client.id)}>
+              {client.name}{' '}
+              <Link to={`/profile/${client.id}`}>
+                <button>Ver Perfil</button>
+              </Link>{' '}
+              {expandedClients[client.id] ? '▲' : '▼'}
+            </button>
+            {expandedClients[client.id] && (
+              <div>
+                <h2>Empresas:</h2>
+                {client.entreprise.map((enterprise, index) => (
+                  <div key={index}>
+                    <button onClick={() => handleEnterpriseAccordionClick(index)}>
+                      {enterprise.razaoSocial}{' '}
+                      {expandedEnterpriseIndex === index ? '▲' : '▼'}
+                    </button>
+                    {expandedEnterpriseIndex === index && (
+                      <div>
+                        <p>CNPJ: {enterprise.cpfCnpj}</p>
+                        <Link to={`/entrepriseProfile/${enterprise.cpfCnpj}`}>
+                          <button>Ver Perfil da Empresa</button>
+                        </Link>
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
+  export default Client;
