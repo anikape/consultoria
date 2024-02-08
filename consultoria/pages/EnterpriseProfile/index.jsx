3649f2064173a75465cb54c48e767091f9f50701
Edvaldo Cabral <cabralnet@gmail.com>
2024-02-08 16:02:17 -0300

    feat: página de perfil da empresa

--- consultoria/pages/EnterpriseProfile/index.jsx
+++ consultoria/pages/EnterpriseProfile/index.jsx
@@ -7,7 +7,6 @@ import { useData } from "../../src/hooks/useData";
 
 const EntrepriseProfile = () => {
   const { id } = useParams();
-  console.log(id);
 
   const { ["data"]: company, loading, error, request } = useData();
 
@@ -41,9 +40,6 @@ const EntrepriseProfile = () => {
   };
 
   // Verifica se a prop "clients" está definida
-  if (!company) {
-    return <h1>Não tem</h1>;
-  }
 
   // Encontra a empresa correspondente ao cpfCnpj
   // const enterprise = clients
@@ -51,12 +47,12 @@ const EntrepriseProfile = () => {
   //   .find((enterprise) => enterprise.cpfCnpj.toString() === cpfCnpj);
 
   // Verifica se a empresa foi encontrada
-  if (!company) {
-    return <h1>Enterprise not found</h1>;
-  }
+  // if (!company) {
+  //   return <h1>Enterprise not found</h1>;
+  // }
 
   return (
-    <div className={style.container}>
+    <main className={style.container}>
       <div className={style.contentContainer}>
         <div className={style.button}>
           <Link to="/client" className={style.buttons}>
@@ -67,67 +63,79 @@ const EntrepriseProfile = () => {
             <button>HOME</button>
           </Link>
         </div>
-        <h1 className={style.title1}>{company.companyName}</h1>
-        {/* Exibir outras informações da empresa */}
 
-        <section className={style.profile}>
-          <div>
-            <h2>CPF/CNPJ</h2>
-            <span>{company.cnpj}</span>
-          </div>
+        {!company && error && (
+          <>
+            <div className={style.errorContainer}>
+              <h1>Empresa não encontrada</h1>
+              {error && "Não foi possivel carregar os dados"}
+            </div>
+          </>
+        )}
 
-          <div>
-            <h2>E-mail</h2>
-            <span>{company.email}</span>
-          </div>
+        {company && !error && (
+          <>
+            <h1 className={style.title1}>{company.companyName}</h1>
 
-          <div>
-            <h2>Telefone Celular</h2>
-            <span>{company.cellprone}</span>
-          </div>
+            <section className={style.profile}>
+              <div className={style.profileItem}>
+                <h2>CPF/CNPJ</h2>
+                <span>{company.cnpj}</span>
+              </div>
 
-          <div>
-            <h2>Telefone Fixo</h2>
-            <span>{company.prone}</span>
-          </div>
+              <div className={style.profileItem}>
+                <h2>E-mail</h2>
+                <span>{company.email}</span>
+              </div>
 
-          <div>
-            <h2>Rua/Logradouro</h2>
-            <span>{company.address}</span>
-          </div>
+              <div className={style.profileItem}>
+                <h2>Telefone Celular</h2>
+                <span>{company.cellphone}</span>
+              </div>
 
-          <div>
-            <h2>Bairro</h2>
-            <span>{company.bairro}</span>
-          </div>
+              <div className={style.profileItem}>
+                <h2>Telefone Fixo</h2>
+                <span>{company.phone}</span>
+              </div>
 
-          <div>
-            <h2>Complemento</h2>
-            <span>{company.complemento}</span>
-          </div>
+              <div className={style.profileItem}>
+                <h2>Rua/Logradouro</h2>
+                <span>{company.address}</span>
+              </div>
 
-          <div>
-            <h2>Cidade</h2>
-            <span>{company.city}</span>
-          </div>
+              <div className={style.profileItem}>
+                <h2>Bairro</h2>
+                <span>{company.bairro}</span>
+              </div>
 
-          <div>
-            <h2>UF</h2>
-            <span>{company.state}</span>
-          </div>
+              <div className={style.profileItem}>
+                <h2>Complemento</h2>
+                <span>{company.complemento}</span>
+              </div>
 
-          <div>
-            <h2>CEP</h2>
-            <span>{company.zipcode}</span>
-          </div>
-        </section>
+              <div className={style.profileItem}>
+                <h2>Cidade</h2>
+                <span>{company.city}</span>
+              </div>
 
-        <section className={style.documents}>
-          <h2>Documentos:</h2>
+              <div className={style.profileItem}>
+                <h2>UF</h2>
+                <span>{company.state}</span>
+              </div>
 
-          {error ? "Não foi possivel carregar os dados" : "dados"}
+              <div className={style.profileItem}>
+                <h2>CEP</h2>
+                <span>{company.zipcode}</span>
+              </div>
+            </section>
 
-          {/* {company.documents.map((document, index) => (
+            <section className={style.documents}>
+              <h2 className={style.title1}>Documentos:</h2>
+            </section>
+          </>
+        )}
+
+        {/* {company.documents.map((document, index) => (
             <div key={index}>
               <button onClick={() => toggleDocumentAccordion(index)}>
                 {document.name} {expandedDocument === index ? "▲" : "▼"}
@@ -139,19 +147,18 @@ const EntrepriseProfile = () => {
                   <Link to={document.path} target="_blank" download>
                     <AiOutlineFilePdf />
                   </Link> */}
-          {/* Opção de exclusão do banco de dados */}
-          {/* <button onClick={() => handleDeleteDocument(document.id)}>
+        {/* Opção de exclusão do banco de dados */}
+        {/* <button onClick={() => handleDeleteDocument(document.id)}>
                     Excluir Documento
                   </button>
                 </div> */}
-          {/* )} */}
-          {/* </div>
+        {/* )} */}
+        {/* </div>
           ))} */}
-        </section>
       </div>
 
       <Footer />
-    </div>
+    </main>
   );
 };
 
