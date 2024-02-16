9250c8453b75b2f218b686b5280f6c1b0a9de7f1
edcabralc <cabralnet@gmail.com>
2024-02-16 09:36:35 -0300

    fix: remoção de rota Profile

--- consultoria/pages/Client/index.jsx
+++ consultoria/pages/Client/index.jsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 
-import Profile from "../Profile";
 import EntrepriseProfile from "../EnterpriseProfile";
 import { FaHome } from "react-icons/fa";
 import DocumentPage from "../DocumentsPage/DocumentsPage";
