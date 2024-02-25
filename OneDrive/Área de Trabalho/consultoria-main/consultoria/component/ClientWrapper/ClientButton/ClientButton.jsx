1f122a5cb42739a0a1a62b992c69a0cc9e5cef52
edcabralc <cabralnet@gmail.com>
2024-02-24 21:36:56 -0300

    fix: yashina merge

--- OneDrive/Área de Trabalho/consultoria-main/consultoria/component/ClientWrapper/ClientButton/ClientButton.jsx
+++
@@ -1,9 +0,0 @@
-import style from "./ClientButton.module.css";
-
-export const ClientButton = ({ client }) => {
-  return (
-    <>
-      <button className={style.buttonName}>{client.name}</button>
-    </>
-  );
-};
