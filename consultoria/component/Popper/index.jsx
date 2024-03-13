9088b6126bef6d7b98eb4767b163eb9225232236
Edvaldo Cabral <cabralnet@gmail.com>
2024-03-13 16:13:23 -0300

    refactor: adicionando contexto ao Popper

--- consultoria/component/Popper/index.jsx
+++ consultoria/component/Popper/index.jsx
@@ -1,7 +1,9 @@
 import { PopperButton } from "./PopperButton";
 import { PopperContent } from "./PopperContent";
+import { PopperBody } from "./PopperBody";
 
 export const Popper = {
   Button: PopperButton,
+  Body: PopperBody,
   Content: PopperContent,
 };
