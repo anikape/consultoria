35a41fa140183420a889d3a79f74fa7835f1311d
edcabralc <cabralnet@gmail.com>
2024-11-28 16:32:28 -0300

    style: fix adm box ui

--- consultoria/src/components/AdminProfile/index.jsx
+++ consultoria/src/components/AdminProfile/index.jsx
@@ -2,31 +2,33 @@ import { useState, useEffect, useContext, useReducer } from "react";
 
 import { AuthContext } from "@contexts/Auth/AuthContext";
 import { useFetch } from "@hooks/useFetch";
-import style from "@components/AdminProfile/AdminProfile.module.css";
 import { useAdmin } from "@hooks/useAdmin";
+import style from "@components/AdminProfile/AdminProfile.module.css";
 
 const AdminProfile = () => {
   const [loading, setLoading] = useState(false);
   const auth = useContext(AuthContext);
-
-  const { admin, addAdmin, removeAdmin, editAdmin, loadAdmin } = useAdmin();
+  const { admin, loadAdmin } = useAdmin();
   const { getData } = useFetch();
 
+  const userId = auth.user?.id;
+
   const loadData = async () => {
+    setLoading(true);
     try {
-      const response = await getData(
-        `admin/${auth.user?.id}`,
-        { password: "admin" },
-        {
-          withCredentials: true,
-        }
-      );
+      const response = await getData(`admin/${userId}`, {
+        withCredentials: true,
+      });
+      console.log(response);
       if (response.status !== 200) {
-        return;
+        throw new Error("Erro ao carregar dados");
       }
-      loadAdmin([response.data]);
+
+      await loadAdmin([response.data]);
+      setLoading(false);
       console.log(response);
     } catch (error) {
+      loadAdmin([]);
       console.log(error);
     }
   };
@@ -34,32 +36,36 @@ const AdminProfile = () => {
     loadData();
   }, []);
   console.log(admin);
+  console.log(auth.user);
+  console.log(admin.length);
+
+  if (loading && !admin) {
+    return <p>Carregando...</p>;
+  }
 
   return (
-    <section className={style.admSection}>
-      <div className={style.admContainer}>
-        <div className={style.admContent}>
-          <div className={style.userData}>
-            {admin?.map(item => (
-              <div key={admin?.id} className={style.userDataItem}>
-                <div>
-                  <p>Nome:</p>
-                  <p>{item.name}</p>
-                </div>
-                <div>
-                  <p>CPF:</p>
-                  <p key={admin.id}>{item.cpf ?? "CPF não cadastrado"}</p>
-                </div>
-                <div>
-                  <p>Email:</p>
-                  <p key={admin.id}>{item.email}</p>
-                </div>
+    <div className={style.userData}>
+      {loading && !admin && <p>Carregando...</p>}
+      <div className={style.userDataInfo}>
+        {!loading &&
+          admin?.map(item => (
+            <div key={admin?.id} className={style.userDataInfoItem}>
+              <div>
+                <p>Nome:</p>
+                <p>{item.name}</p>
               </div>
-            ))}
-          </div>
-        </div>
+              <div>
+                <p>CPF:</p>
+                <p>{item.cpf ?? "CPF não cadastrado"}</p>
+              </div>
+              <div>
+                <p>Email:</p>
+                <p>{item.email}</p>
+              </div>
+            </div>
+          ))}
       </div>
-    </section>
+    </div>
   );
 };
 
--- consultoria/src/pages/Adm/index.jsx
+++ consultoria/src/pages/Adm/index.jsx
@@ -1,4 +1,5 @@
-import { Link, Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { AdminProvider } from "@contexts/Admin/AdminContext";
 import { HiUser, HiUserPlus } from "react-icons/hi2";
@@ -6,60 +7,87 @@ import { FaUserEdit } from "react-icons/fa";
 
 import home from "@/assets/home.png";
 import style from "@pages/Adm/adm.module.css";
+// import axios from "axios";
 
 const Index = () => {
+  // const [authorized, setAuthorized] = useState(true);
+  // const navigate = useNavigate();
+
+  // const checkAuthorization = async () => {
+  //   try {
+  //     const response = await axios.get("", { withCredentials: true });
+  //     if (response.status !== 200) {
+  //       throw new Error("Erro ao autorizar acesso");
+  //     }
+
+  //     setAuthorized(true);
+  //   } catch (error) {
+  //     setAuthorized(false);
+  //     navigate("/AdminAuth");
+  //     console.log(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   checkAuthorization();
+  // }, [navigate]);
+
+  // if (!authorized) {
+  //   return <p>Carregando...</p>;
+  // }
+
   return (
     <AdminProvider>
       <section className={style.admSection}>
         <div className={style.admContainer}>
+          <div className={style.admNav}>
+            <nav className={style.nav}>
+              <ul className={style.navLinks}>
+                <li>
+                  <Link className={style.links} to="/Home">
+                    <img src={home} alt="Home" />
+                    <p className={style.linkText}>Home</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link className={style.links} to={"/Adm"}>
+                    <div className={style.linkIcon}>
+                      <HiUser />
+                    </div>
+                    <p className={style.linkText}>Perfil</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"settings"} className={style.links}>
+                    <div className={style.linkIcon}>
+                      <FaUserEdit />
+                    </div>
+
+                    <p className={style.linkText}>Editar</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link className={style.links} to={"change-password"}>
+                    <div className={style.linkIcon}>
+                      <RiLockPasswordFill />
+                    </div>
+                    <p className={style.linkText}>Alterar senha</p>
+                  </Link>
+                </li>
+
+                <li>
+                  <Link className={style.links} to="/CadastroAdm">
+                    <div className={style.linkIcon}>
+                      <HiUserPlus />
+                    </div>
+
+                    <p className={style.linkText}>Novo usuário</p>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
           <div className={style.admContent}>
-            <div className={style.admNav}>
-              <nav className={style.nav}>
-                <ul className={style.navLinks}>
-                  <li>
-                    <Link className={style.links} to="/Home">
-                      <img src={home} alt="Home" />
-                      <p className={style.linkText}>Home</p>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className={style.links} to={"/Adm"}>
-                      <div className={style.linkIcon}>
-                        <HiUser />
-                      </div>
-                      <p className={style.linkText}>Perfil</p>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to={"settings"} className={style.links}>
-                      <div className={style.linkIcon}>
-                        <FaUserEdit />
-                      </div>
-
-                      <p className={style.linkText}>Editar</p>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className={style.links} to={"change-password"}>
-                      <div className={style.linkIcon}>
-                        <RiLockPasswordFill />
-                      </div>
-                      <p className={style.linkText}>Alterar senha</p>
-                    </Link>
-                  </li>
-
-                  <li>
-                    <Link className={style.links} to="/CadastroAdm">
-                      <div className={style.linkIcon}>
-                        <HiUserPlus />
-                      </div>
-
-                      <p className={style.linkText}>Novo usuário</p>
-                    </Link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
             <Outlet />
           </div>
         </div>
