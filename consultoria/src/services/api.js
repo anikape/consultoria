a699897edb6450c71bbf987450c3e8965f2563f6
edcabralc <cabralnet@gmail.com>
2024-12-09 14:25:53 -0300

    fix: remove proxy request for api

--- consultoria/src/services/api.js
+++ consultoria/src/services/api.js
@@ -1,31 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "/api",
-  // baseURL: import.meta.env.VITE_BASE_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  withCredentials: true,
-  proxy: {
-    protocol: "http",
-    host: import.meta.env.VITE_BASE_API_URL,
-  },
-});
-
-export const http = axios.create({
   baseURL: import.meta.env.VITE_BASE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
   withCredentials: true,
-  proxy: {
-    protocol: "http",
-    host: import.meta.env.VITE_BASE_API_URL,
-    // port: 9000,
-    // auth: {
-    //   username: "mikeymike",
-    //   password: "rapunz3l",
-    // },
-  },
 });
